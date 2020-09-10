2b7cd59b7cc3f3601a67a2e12f0f1ca5535081c7
luisescobar89 <luis.scobr@gmail.com>
2020-09-10 11:56:40 -0500

    add dt groovy file

---
+++ dynatrace.groovy
@@ -0,0 +1,108 @@
+package dynatrace
+
+def dynatracePushCustomInfoEvent (Map args){
+  // check input arguments
+  String dtTenantUrl = args.containsKey("dtTenantUrl") ? args.dtTenantUrl : "${DT_TENANT_URL}"
+  String dtApiToken = args.containsKey("dtApiToken") ? args.dtApiToken : "${DT_API_TOKEN}"
+  def tagRule = args.containsKey("tagRule") ? args.tagRule : ""
+  String source = args.containsKey("source") ? args.source : "Jenkins"
+  String description = args.containsKey("description") ? args.description : ""
+  String title = args.containsKey("title") ? args.title : ""
+
+  def customProperties = args.containsKey("customProperties") ? args.customProperties : [ ]
+
+if ((dtTenantUrl == "") || (dtApiToken == "")) {
+    echo "dynatracePushCustomInfoEvent requires dynatrace tenant URL/API token!"
+    return false;
+}
+
+  // check minimum required params
+  if( (description == "")) || (source == ""))  {
+    echo "dynatracePushCustomInfoEvent requires source and description to be set. These values cant be empty!"
+    return 1
+  }
+
+String eventType = "CUSTOM_INFO"
+
+def createEventBody = """{
+    "eventType": "${eventType},
+    "attachRules": {
+        "tagRule": "${tagRule}"
+        },
+    "description: "${deploymentName}",
+    "title": "${deploymentVersion}",
+    "source": "${source}"
+    "customProperties": "${customProperties}"
+}"""
+
+def createEventResponse = httpRequest contentType: 'APPLICATION_JSON', 
+    customHeaders: [[maskValue: true, name: 'Api-Token ', value: "${dtApiToken}"]], 
+    httpMode: 'POST',
+    requestBody: createEventBody,
+    responseHandle 'STRING',
+    url: "${dtTenantUrl}/api/v1/events",
+    validResponseCodes: "200",
+    ignoreSslErrors: true
+
+    if (createEventResponse.status == 200) {
+        echo "Deployment event posted successfully!"
+    } else {
+        echo "Failed To post event:" + createEventResponse.content          
+    }
+
+}
+
+def dynatracePushDeployEvent (Map args){
+  String dtTenantUrl = args.containsKey("dtTenantUrl") ? args.dtTenantUrl : "${DT_TENANT_URL}"
+  String dtApiToken = args.containsKey("dtApiToken") ? args.dtApiToken : "${DT_API_TOKEN}"
+  def tagRule = args.containsKey("tagRule") ? args.tagRule : ""
+  String source = args.containsKey("source") ? args.source : "Jenkins"
+  String deploymentName = args.containsKey("deploymentName") ? args.deploymentName : "${env.JOB_NAME}"
+  String deploymentVersion = args.containsKey("deploymentVersion") ? args.deploymentVersion : "${env.VERSION}"
+  String deploymentProject = args.containsKey("deploymentProject") ? args.deploymentProject : ""
+  String ciBackLink = args.containsKey("ciBackLink") ? args.ciBackLink : "${env.BUILD_URL}"
+  String remediationAction = args.containsKey("remediationAction") ? args.remediationAction : "null"
+  def customProperties = args.containsKey("customProperties") ? args.customProperties : [ ]
+
+if ((dtTenantUrl == "") || (dtApiToken == "")) {
+    echo "Missing dynatrace tenant URL/API token!"
+    return false;
+}
+
+if ((source == "") || (deploymentName == "") || (deploymentVersion == "")) {
+    echo "dynatracePushDeployEvent requires source, deploymentName and deploymentVersion to be set. These values cant be empty!"
+    return false;
+}
+
+String eventType = "CUSTOM_DEPLOYMENT"
+
+def createEventBody = """{
+    "eventType": "${eventType},
+    "attachRules": {
+        "tagRule": "${tagRule}"
+        },
+    "deploymentName": "${deploymentName}",
+    "deploymentVersion": "${deploymentVersion}",
+    "deploymentProject": "${deploymentProject}",
+    "ciBackLink": "${ciBackLink}",
+    "remediationAction": "${remediationAction}",
+    "source": "${source}"
+    "customProperties": "${customProperties}"
+}"""
+
+def createEventResponse = httpRequest contentType: 'APPLICATION_JSON', 
+    customHeaders: [[maskValue: true, name: 'Api-Token ', value: "${dtApiToken}"]], 
+    httpMode: 'POST',
+    requestBody: createEventBody,
+    responseHandle 'STRING',
+    url: "${dtTenantUrl}/api/v1/events",
+    validResponseCodes: "200",
+    ignoreSslErrors: true
+
+    if (createEventResponse.status == 200) {
+        echo "Deployment event posted successfully!"
+    } else {
+        echo "Failed To post event:" + createEventResponse.content          
+    }
+
+}
